6633262471dc94feca286874f322ba715ed783ac
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2022-03-08 17:15:55 +0000

    adicionado redis adapter

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const socketio = require('socket.io');
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+const { createClient } = require("redis");
+const redisAdapter = require("@socket.io/redis-adapter");
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+(async () => {
+  io = socketio(http, { cors: { origins: ['*'], methods: ['GET', 'POST'] }, transports: ['websocket'] });
+
+  const pubClient = createClient({ url: "redis://127.0.0.1:6379" });
+  const subClient = pubClient.duplicate();
+
+  io.adapter(redisAdapter(pubClient, subClient));
+
+  await pubClient.connect();
+  await subClient.connect();
+  
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
   });
-});
+  
+  io.on('connection', async (socket) => {
+    await iniciaKeyRedis(pubClient);
+    let hash = await criaUsuario(pubClient, socket.id);
 
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
+    socket.emit('chat message', `User cluster pID = ${process.pid}, socket ID = ${socket.id}, hash = ${hash}`);
+    socket.join("/");
+
+    socket.on('chat message', async msg => {
+      if (msg.includes("disconnect")) {
+        const [fn, hash] = msg.split(" ");
+        await desconectaUsuario(pubClient, io, hash);
+      }
+
+      io.emit('chat message', msg);
+    });
+  });
+  
+  http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+  });
+})();
+
+async function iniciaKeyRedis(pubClient) {
+  if (await pubClient.get('users') === null) {
+    await pubClient.set('users', JSON.stringify({}));
+  }
+}
+
+async function criaUsuario(pubClient, socketid) {
+  const users = JSON.parse(await pubClient.get('users'));
+  const hash = Math.floor(Math.random() * 101);
+  const user = {
+    hash: hash,
+    socketid: socketid
+  };
+  users[hash] = user;
+  await pubClient.set('users', JSON.stringify(users));
+  return hash;
+}
+
+async function desconectaUsuario(pubClient, io, hash) {
+  const users = JSON.parse(await pubClient.get('users'));
+  const socketid = users[hash].socketid;
+  io.of("/").adapter.remoteDisconnect(socketid, true);
+  delete users[hash];
+  await pubClient.set('users', JSON.stringify(users));
+}
